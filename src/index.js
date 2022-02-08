73e433706d6512dad4e8e2e7552ae9863ca24a3c
EugeneNorko <zhenia020221@gmail.com>
2022-02-08 11:32:25 +0200

    Рефакторинг кода

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+
+import './scripts/button-back/btn-back';
+
+import './scripts/mobile-menu/mobile.js';
+
+import './scripts/about/modal-about.js';
+import './scripts/product/products-mod';
+import './scripts/product/products-mod2';
+import './scripts/product/products-mod3';
+import './scripts/customer/slider.js';
+import './scripts/contacts/contacts-btn.js';
+
+
+
+
+
+
