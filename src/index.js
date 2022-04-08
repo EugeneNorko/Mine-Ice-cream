ae32300b6d12ece9e6c256c8c83d0434c8323293
EugeneNorko <zhenia020221@gmail.com>
2022-04-08 12:31:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import './scripts/button-back/btn-back';
 import './scripts/mobile-menu/mobile.js';
 
 import './scripts/about/modal-about.js';
-import './scripts/product/products-card1';
-import './scripts/product/products-card2';
-import './scripts/product/products-card3';
 import './scripts/customer/slider.js';
 import './scripts/contacts/contacts-our-location.js';
 import './scripts/contacts/contacts-franchise.js';
